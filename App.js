792af8915cf7afeb477c53c825ca37ba8fe0fbb4
Ankit <ankit@sahusodt.com>
2017-08-04 15:25:32 +0530

    Added Firebase

--- App.js
+++ App.js
@@ -15,12 +15,16 @@ export default class App extends React.Component {
   async componentWillMount() {
     await Expo.Font.loadAsync({
       Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
     });
     this.setState({ isReady: "true" });
   }
   render() {
-    return <TyperScreen />;
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
+    return <SurveyScreen />;
   }
 }
 
--- src/SurveyScreen.js
+++ src/SurveyScreen.js
@@ -12,8 +12,44 @@ import {
   Text,
   Body
 } from "native-base";
+
+import * as firebase from 'firebase';
+import { firebaseConfig } from './config/firebase';
+
+firebase.initializeApp(firebaseConfig);
+
 export default class FloatingLabelExample extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      name: ''
+    }
+    this._onSubmitClick = this._onSubmitClick.bind(this);
+  }
+
+  _onSubmitClick() {
+    firebase.database().ref('Users/' + this.state.name).set({
+      name: this.state.name,
+      age: 22,
+      Occupation: 'Software Developer'
+    });
+
+  }
+  componentDidMount() {
+                // Firebase
+    firebase.database().ref('Questions/').on('value', (snapshot) => {
+      const highscore = snapshot.val();
+      console.log( highscore);
+    });
+                //Questions Listener
+    // firebase.database().ref('Questions/').on('value', (snapshot) => {
+    //   const questions = snapshot.val();
+    //   console.log(questions);
+    // });
+  }
   render() {
+    console.log(this.state.name);
     return (
       <Container>
         <Header>
@@ -25,7 +61,7 @@ export default class FloatingLabelExample extends Component {
           <Form>
             <Item floatingLabel>
               <Label>Name</Label>
-              <Input />
+              <Input onChangeText={(val) => this.setState({ name: val })}/>
             </Item>
             <Item floatingLabel last>
               <Label>Age</Label>
@@ -41,7 +77,7 @@ export default class FloatingLabelExample extends Component {
             </Item>
           </Form>
           <View style={{ padding: 10 }}>
-            <Button block dark style={{ marginTop: 20 }}>
+            <Button block dark style={{ marginTop: 20 }} onPress={() => this._onSubmitClick()}>
               <Text>Submit</Text>
             </Button>
           </View>
