9303f058861c2ec9976e75b8b3d70e21277fdc32
Ankit <ankit@sahusodt.com>
2017-08-04 15:26:02 +0530

    Added Expo File

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAG7CxWol6qxwumpXgacaS3IHiKJLHz4kM",
+    authDomain: "surveyapp-7dbb7.firebaseapp.com",
+    databaseURL: "https://surveyapp-7dbb7.firebaseio.com",
+    projectId: "surveyapp-7dbb7",
+    storageBucket: "",
+    messagingSenderId: "245740130511"
+  };
